e4dd740a3c06d2b7dfdee580fc8c68822135cf31
frankbonafide <frlud6323@usn.no>
2025-10-20 12:28:29 +0200

    Add initial HTML structure for administration system

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>Vedlikeholdssystem for student og klasse</title>
+</head>
+<body>
+    <h1>Administrasjonssystem</h1>
+
+    <h2>Klasse</h2>
+    <ul>
+        <li><a href="klasse/registrer_klasse.php">Registrer klasse</a></li>
+        <li><a href="klasse/vis_klasser.php">Vis alle klasser</a></li>
+        <li><a href="klasse/slett_klasse.php">Slett klasse</a></li>
+    </ul>
+
+    <h2>Student</h2>
+    <ul>
+        <li><a href="student/registrer_student.php">Registrer student</a></li>
+        <li><a href="student/vis_studenter.php">Vis alle studenter</a></li>
+        <li><a href="student/slett_student.php">Slett student</a></li>
+    </ul>
+</body>
+</html>
