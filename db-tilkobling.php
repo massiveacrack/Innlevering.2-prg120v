a210982df1b0932e5226ca12defaf18e401c09f7
frankbonafide <frlud6323@usn.no>
2025-10-20 12:27:57 +0200

    Add database connection script in db-tilkobling.php

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -1,1 +1,12 @@
+<?php
+$server = "localhost";
+$bruker = "root";
+$passord = "";
+$dbnavn = "skole"; // du kan kalle databasen hva du vil
 
+$conn = new mysqli($server, $bruker, $passord, $dbnavn);
+
+if ($conn->connect_error) {
+    die("Feil ved tilkobling: " . $conn->connect_error);
+}
+?>
